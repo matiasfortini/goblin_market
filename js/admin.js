7aea65cda28fe937625df28ebd81bf53879fef4e
matiasfortini <matiasfortini88@gmail.com>
2023-05-17 15:39:04 -0300

    Login admin falta modal error

---
+++ js/admin.js
@@ -0,0 +1,26 @@
+const admins = [
+    {
+        user: "matiasfortini",
+        password: "matiasfortini1988&"
+    }
+]
+
+const loginValidation = () => {
+    const loginForm = document.querySelector("#loginForm")
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const usernameInput = document.querySelector("#usernameInput").value
+        const passwordInput = document.querySelector("#passwordInput").value
+
+        const foundAdmin = admins.find((admin) => admin.user === usernameInput && admin.password === passwordInput)
+
+        if (foundAdmin) {
+            window.location.href = "../../pages/admin.html"
+        }else{
+            console.log("problemas")
+        }
+    })
+}
+
+loginValidation()
