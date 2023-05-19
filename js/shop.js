2df279ba6b6c9a706a6143f5bea46a085586d472
matiasfortini <matiasfortini88@gmail.com>
2023-05-19 09:23:40 -0300

    los productos ya se agregan al shop automaticamente

--- js/shop.js
+++ js/shop.js
@@ -11,17 +11,32 @@ const viewProductsShop = () => {
         const productCard = document.createElement("div");
         productCard.className = "productCard";
         productCard.innerHTML = `
-            <div class="imgProduct">
-                <a href="#"><img src="${product.img}" alt="Remera The Amazin Ninja Dude!"></a>
-            </div>
-            <div class="infoProduct">
-                <a href="#"><h2>${product.name}</h2></a>
-                <p>$${product.price}</p>
-            </div>
+                                <div class="imgProduct">
+                                    <a href="#">
+                                        <img src="${product.img}" alt="${product.brand} ${product.type} ${product.name}">
+                                    </a>
+                                </div>
+                                <div class="infoProduct">
+                                    <a href="#"><h2>${product.name}</h2></a>
+                                    <p>$${product.price}</p>
+                                </div>
+
+            
         `;
 
         productsShop.append(productCard);
     });
 };
 
+
+
 viewProductsShop();
+
+
+/*<div class="imgProduct">
+                <a href="#"><img src="#" alt="${product.brand} ${product.type} ${product.name}"></a>
+            </div>
+            <div class="infoProduct">
+                <a href="#"><h2>${product.name}</h2></a>
+                <p>$${product.price}</p>
+            </div>*/
